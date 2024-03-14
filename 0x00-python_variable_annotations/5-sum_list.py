d47db8313d91989277682bde9157184784c6fbe5
Junior Ohanyere <junohanyere@gmail.com>
2024-03-14 14:50:05 +0100

    sum of list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''sum of lists of floats
+'''
+
+
+def sum_list(input_list: list[float] = []) -> float:
+    '''get the sum of lists of floats passed as argument
+
+    Args:
+        input_list: the list of floats
+
+    Return:
+        return the sum of the list of the floats
+    '''
+
+    return sum(input_list)
